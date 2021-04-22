ea7286ce9cf366f73e8912b17fa778ccceeb0d3a
Jay Hastings <jayhjman@gmail.com>
2021-04-21 21:47:42 -0700

    added total by cust type and gender pie

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template, redirect, send_from_directory
 
 import os
+import json
+
+from pprint import pprint
 
 from db_info import db_user, db_password, db_name
 
@@ -67,7 +70,7 @@ def products():
     session = Session(engine)
 
     """Return a list of a dicionary for mortalites by state"""
-    # Query all for mortality
+    # Query all for distinct products
     query = session.query(func.distinct(Supermarket.product_line))
 
     # debug query
@@ -90,7 +93,66 @@ def products():
 
 @app.route(f"{ver_str}/genderinvoices")
 def gender_invoices():
-    return jsonify({})
+
+    session = Session(engine)
+    query = session.query(Supermarket.gender, 
+        func.count(Supermarket.invoice_id))
+    query = query.group_by(Supermarket.gender)
+
+    # debug query
+    print(query.statement.compile())
+
+    # Get all the results
+    results = query.all()
+
+    session.close()  
+
+    gender_list = []
+    total_list = []
+    for row in results:
+        gender_list.append(row[0])
+        total_list.append(row[1])
+
+    gender_dict = {}
+    gender_dict = {
+        "gender": gender_list,
+        "total": total_list
+    }
+
+    return jsonify(gender_dict)
+
+@app.route(f"{ver_str}/customertypetotal")
+def customertype_total():
+
+    session = Session(engine)
+    query = session.query(Supermarket.customer_type, 
+        Supermarket.gender, func.sum(Supermarket.total))
+    query = query.group_by(Supermarket.customer_type, 
+        Supermarket.gender)
+
+    # debug query
+    print(query.statement.compile())
+
+    # Get all the results
+    results = query.all()
+
+    session.close()  
+
+    customer_type_list = []
+    total_list = []
+    for row in results:
+        customer_type_list.append(row[0] + " " + row[1])
+        total_list.append(float(row[2]))
+
+    customer_type_dict = {
+        "customer_type": customer_type_list,
+        "total": total_list
+    }
+
+    pprint(customer_type_dict)
+
+    return jsonify(customer_type_dict)
+
 
 
 #
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,29 +1,64 @@
 apiVersion = "/v1.0"
 genderInvoiceURL = apiVersion + "/genderinvoices"
-
+customerTypeTotalURL= apiVersion + "/customertypetotal"
 function loadData() {
+  
   d3.json(genderInvoiceURL).then((pieData) => {
     console.log("gender invoices:");
     console.log(pieData);
-    drawPie();
+    drawGenderPie(pieData["gender"], pieData["total"]);
   });
+
+  d3.json(customerTypeTotalURL).then((pieData) => {
+    console.log("gender invoices:");
+    console.log(pieData);
+    drawCustomerTypePie(pieData["customer_type"], pieData["total"]);
+  });
+
 }
 
-function drawPie() {
-  var data = [
-    {
-      values: [19, 26, 55],
-      labels: ["Residential", "Non-Residential", "Utility"],
-      type: "pie",
+function drawGenderPie(gender, total) {
+
+  var trace = {
+    values: total,
+    labels: gender,
+    pull: gender.map((x) => 0.05),
+    type: "pie",
+    rotation: 45,
+    marker: {
+        colors: ["red", "blue"],
     },
-  ];
+  } 
+
+  var data = [trace];
 
   var layout = {
-    height: 400,
+    height: 500,
     width: 500,
+    title: "Distribution Of Invoices By Gender",
   };
 
   Plotly.newPlot("genderPie", data, layout);
 }
 
+function drawCustomerTypePie(customerType, total) {
+
+  var trace = {
+    values: total,
+    labels: customerType,
+    pull: customerType.map((x) => 0.05),
+    type: "pie",
+    rotation: 45,
+  } 
+
+  var data = [trace];
+
+  var layout = {
+    height: 500,
+    width: 500,
+    title: "Customer Type and Gender by Purchase Total",
+  };
+
+  Plotly.newPlot("customerTypePie", data, layout);
+}
 loadData();
